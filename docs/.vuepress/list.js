0dc2f660e79ae0cf8ea0a60bc009e9a9128c8c8c
Ahram Kim <ahram0223@gmail.com>
2024-01-27 21:36:09 +0900

    2024.01.27

---
+++ docs/.vuepress/list.js
@@ -0,0 +1,65 @@
+// exports.BaekJoonList = [
+    
+// ];
+// exports.FlutterList = [
+//     "flutter/2023-06-11-how-to-start-flutter-project.md"
+// ];
+// exports.GsapList = [
+//     "gsap/2023-06-27-gsap-basic-tween.md",
+//     "gsap/2023-06-29-gsap-basic-timelines.md",
+//     "gsap/2023-07-06-gsap-basic-buttons-effects.md",
+//     "gsap/2023-07-11-gsap-basic-final-project.md",
+//     "gsap/2023-07-13-gsap-basic-keyframes.md"
+// ];
+exports.HtmlCssList = [
+    "html-css/2022-12-15-doctype.md",
+    "html-css/2023-01-16-cascading-system.md",
+    "html-css/2023-01-17-css-selector.md",
+    "html-css/2023-01-18-bfc-ifc.md",
+    "html-css/2023-01-18-box-model.md",
+    "html-css/2023-01-26-float.md",
+    "html-css/2023-01-26-z-index.md",
+    "html-css/2023-01-31-reset-normalize.md",
+]
+// exports.InterviewList = [
+    
+// ];
+// exports.ITKnowledgeList = [
+//     "IT-Knowledge/2022-09-01-readme-emoji.md",
+//     "IT-Knowledge/2022-11-21-dbms.md",
+//     "IT-Knowledge/2022-11-22-url-protocol-tcp-ip.md",
+//     "IT-Knowledge/2022-11-22-web-service-processing.md",
+//     "IT-Knowledge/2022-11-23-frontend-backend.md",
+//     "IT-Knowledge/2022-11-23-osi-7-layer.md",
+//     "IT-Knowledge/2022-11-24-msa.md",
+//     "IT-Knowledge/2022-11-24-mvc-pattern.md",
+//     "IT-Knowledge/2022-11-29-docker.md",
+//     "IT-Knowledge/2022-11-30-http-method.md",
+//     "IT-Knowledge/2022-12-02-cloud.md",
+//     "IT-Knowledge/2022-12-05-git.md",
+//     "IT-Knowledge/2022-12-07-cookie-session.md",
+//     "IT-Knowledge/2022-12-07-server-side-script.md",
+//     "IT-Knowledge/2022-12-09-api.md",
+//     "IT-Knowledge/2022-12-22-web-standard.md",
+// ];
+// exports.JavaScriptList = [
+//     "javascript/2021-webs-study-quiz.md",
+//     "javascript/2022-webs-study-quiz.md",
+//     "javascript/2022-09-30-dream-coding-learn-js.md",
+//     "javascript/2022-10-12-nomad-coder-chrome-app.md",
+//     "javascript/2022-12-02-ajax.md",
+//     "javascript/2022-12-07-ternary-operator",
+//     "javascript/2022-12-08-json",
+//     "javascript/2022-12-09-closure-anonymous",
+//     "javascript/2022-12-20-element-attribute",
+//     "javascript/2022-12-22-window-dom-bom",
+// ];
+// exports.LaravelList = [
+//     "laravel/2022-12-03-how-to-start-laravel-project.md"
+// ];
+// exports.ProgrammersList = [
+
+// ];
+// exports.ReactList = [
+//     "react/2022-09-17-how-to-start-react-project.md"
+// ];
