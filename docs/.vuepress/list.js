86b413d086bd86da8ed053c3ffd00081bc0175f9
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-03-14 16:36:04 +0900

    2024.03.14

--- docs/.vuepress/list.js
+++ docs/.vuepress/list.js
@@ -1,55 +1,55 @@
 /** HTML */
 exports.HtmlList = [
-    "html/2022-12-15-doctype.md",
+    "HTML/2022-12-15-doctype.md",
 ],
 
 /** CSS */
 exports.CssList = [
-    "css/2023-01-16-cascading-system.md",
-    "css/2023-01-17-css-selector.md",
-    "css/2023-01-18-bfc-ifc.md",
-    "css/2023-01-18-box-model.md",
-    "css/2023-01-26-float.md",
-    "css/2023-01-26-z-index.md",
-    "css/2023-01-31-reset-normalize.md",
+    "CSS/2023-01-16-cascading-system.md",
+    "CSS/2023-01-17-css-selector.md",
+    "CSS/2023-01-18-bfc-ifc.md",
+    "CSS/2023-01-18-box-model.md",
+    "CSS/2023-01-26-float.md",
+    "CSS/2023-01-26-z-index.md",
+    "CSS/2023-01-31-reset-normalize.md",
 ],
 
 /** JavaScript */
 exports.JavaScriptList = [
-    "javascript/2021-webs-study-quiz.md",
-    "javascript/2022-webs-study-quiz.md",
-    "javascript/2022-09-30-dream-coding-learn-js.md",
-    "javascript/2022-10-12-nomad-coder-chrome-app.md",
-    "javascript/2022-12-02-ajax.md",
-    "javascript/2022-12-07-ternary-operator",
-    "javascript/2022-12-08-json",
-    "javascript/2022-12-09-closure-anonymous",
-    "javascript/2022-12-20-element-attribute",
-    "javascript/2022-12-22-window-dom-bom",
+    "JavaScript/2021-webs-study-quiz.md",
+    "JavaScript/2022-webs-study-quiz.md",
+    "JavaScript/2022-09-30-dream-coding-learn-js.md",
+    "JavaScript/2022-10-12-nomad-coder-chrome-app.md",
+    "JavaScript/2022-12-02-ajax.md",
+    "JavaScript/2022-12-07-ternary-operator",
+    "JavaScript/2022-12-08-json",
+    "JavaScript/2022-12-09-closure-anonymous",
+    "JavaScript/2022-12-20-element-attribute",
+    "JavaScript/2022-12-22-window-dom-bom",
 ];
 
 /** GSAP */
 // exports.GsapList = [
-//     "gsap/2023-06-27-gsap-basic-tween.md",
-//     "gsap/2023-06-29-gsap-basic-timelines.md",
-//     "gsap/2023-07-06-gsap-basic-buttons-effects.md",
-//     "gsap/2023-07-11-gsap-basic-final-project.md",
-//     "gsap/2023-07-13-gsap-basic-keyframes.md"
+//     "GSAP/2023-06-27-gsap-basic-tween.md",
+//     "GSAP/2023-06-29-gsap-basic-timelines.md",
+//     "GSAP/2023-07-06-gsap-basic-buttons-effects.md",
+//     "GSAP/2023-07-11-gsap-basic-final-project.md",
+//     "GSAP/2023-07-13-gsap-basic-keyframes.md"
 // ];
 
 /** React */
 exports.ReactList = [
-    "react/2022-09-17-how-to-start-react-project.md"
+    "React/2022-09-17-how-to-start-react-project.md"
 ];
 
 /** Vue */
 exports.VueList = [
-    "vue/2024-03-04-coding-apple.md",
+    "Vue/2024-03-04-coding-apple.md",
 ];
 
 /** Php */
 exports.LaravelList = [
-    "laravel/2022-12-03-how-to-start-laravel-project.md"
+    "Laravel/2022-12-03-how-to-start-laravel-project.md"
 ];
 
 //** Flutter */
@@ -59,22 +59,22 @@ exports.FlutterList = [
 
 /** IT-Knowledge */
 exports.ITKnowledgeList = [
-    "IT-Knowledge/2022-09-01-readme-emoji.md",
-    "IT-Knowledge/2022-11-21-dbms.md",
-    "IT-Knowledge/2022-11-22-url-protocol-tcp-ip.md",
-    "IT-Knowledge/2022-11-22-web-service-processing.md",
-    "IT-Knowledge/2022-11-23-frontend-backend.md",
-    "IT-Knowledge/2022-11-23-osi-7-layer.md",
-    "IT-Knowledge/2022-11-24-msa.md",
-    "IT-Knowledge/2022-11-24-mvc-pattern.md",
-    "IT-Knowledge/2022-11-29-docker.md",
-    "IT-Knowledge/2022-11-30-http-method.md",
-    "IT-Knowledge/2022-12-02-cloud.md",
-    "IT-Knowledge/2022-12-05-git.md",
-    "IT-Knowledge/2022-12-07-cookie-session.md",
-    "IT-Knowledge/2022-12-07-server-side-script.md",
-    "IT-Knowledge/2022-12-09-api.md",
-    "IT-Knowledge/2022-12-22-web-standard.md",
+    "ComputerScience/2022-09-01-readme-emoji.md",
+    "ComputerScience/2022-11-21-dbms.md",
+    "ComputerScience/2022-11-22-url-protocol-tcp-ip.md",
+    "ComputerScience/2022-11-22-web-service-processing.md",
+    "ComputerScience/2022-11-23-frontend-backend.md",
+    "ComputerScience/2022-11-23-osi-7-layer.md",
+    "ComputerScience/2022-11-24-msa.md",
+    "ComputerScience/2022-11-24-mvc-pattern.md",
+    "ComputerScience/2022-11-29-docker.md",
+    "ComputerScience/2022-11-30-http-method.md",
+    "ComputerScience/2022-12-02-cloud.md",
+    "ComputerScience/2022-12-05-git.md",
+    "ComputerScience/2022-12-07-cookie-session.md",
+    "ComputerScience/2022-12-07-server-side-script.md",
+    "ComputerScience/2022-12-09-api.md",
+    "ComputerScience/2022-12-22-web-standard.md",
 ];
 
 exports.BaekJoonList = [
